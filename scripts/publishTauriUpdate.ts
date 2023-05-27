7a21bdb62ab44d870028f1e71d55b392790ff170
Vigo Vlugt <vigovlugt@gmail.com>
2023-05-27 10:34:42 +0200

    New release asset pipeline to S3

--- scripts/publishTauriUpdate.ts
+++ scripts/publishTauriUpdate.ts
@@ -1,44 +1,72 @@
 import { Octokit } from "@octokit/rest";
 import { config } from "dotenv";
+import {
+    PutObjectCommand,
+    PutObjectCommandInput,
+    S3Client,
+} from "@aws-sdk/client-s3";
+config();
+
+const errors: string[] = [];
+
+const [
+    GITHUB_TOKEN,
+    GIST_TOKEN,
+    GIST_ID,
+    REPOSITORY_NAME,
+    REPOSITORY_OWNER,
+    S3_ACCESS_KEY_ID,
+    S3_SECRET_ACCESS_KEY,
+    S3_ENDPOINT,
+    S3_PUBLIC_URL,
+] = (
+    [
+        "GITHUB_TOKEN",
+        "GIST_TOKEN",
+        "GIST_ID",
+        "REPOSITORY_NAME",
+        "REPOSITORY_OWNER",
+        "S3_ACCESS_KEY_ID",
+        "S3_SECRET_ACCESS_KEY",
+        "S3_ENDPOINT",
+        "S3_PUBLIC_URL",
+    ] as const
+).map((v) => {
+    const value = process.env[v];
+    if (!value) {
+        errors.push(`${v} is not set`);
+    }
+    return value ?? "";
+});
+
+const S3_BUCKET = process.env.S3_BUCKET || "draftgap";
+
+if (errors.length > 0) {
+    console.error(errors.join("\n"));
+    process.exit(1);
+}
+
+const octokit = new Octokit({
+    auth: GITHUB_TOKEN,
+});
+
+// Api token is stored in the environment variable
+export const client = new S3Client({
+    endpoint: S3_ENDPOINT,
+    region: process.env.S3_REGION || "auto",
+    credentials: {
+        accessKeyId: S3_ACCESS_KEY_ID,
+        secretAccessKey: S3_SECRET_ACCESS_KEY,
+    },
+});
 
 export async function main() {
-    config();
-
-    const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-    if (!GITHUB_TOKEN) {
-        throw new Error("GITHUB_TOKEN is not set");
-    }
-
-    const GIST_TOKEN = process.env.GIST_TOKEN;
-    if (!GIST_TOKEN) {
-        throw new Error("GIST_TOKEN is not set");
-    }
-
-    const GIST_ID = process.env.GIST_ID;
-    if (!GIST_ID) {
-        throw new Error("GIST_ID is not set");
-    }
-
-    const REPOSITORY_NAME = process.env.REPOSITORY_NAME;
-    if (!REPOSITORY_NAME) {
-        throw new Error("REPOSITORY_NAME is not set");
-    }
-
-    const REPOSITORY_OWNER = process.env.REPOSITORY_OWNER;
-    if (!REPOSITORY_OWNER) {
-        throw new Error("REPOSITORY_OWNER is not set");
-    }
-
-    const octokit = new Octokit({
-        auth: GITHUB_TOKEN,
-    });
-
     const latestRelease = await octokit.repos.getLatestRelease({
         owner: REPOSITORY_OWNER,
         repo: REPOSITORY_NAME,
     });
 
-    const latestJson = await octokit.repos.getReleaseAsset({
+    const latestJsonBinary = await octokit.repos.getReleaseAsset({
         owner: REPOSITORY_OWNER,
         repo: REPOSITORY_NAME,
         asset_id: latestRelease.data.assets.find(
@@ -50,8 +78,21 @@ export async function main() {
     });
 
     const latestJsonString = new TextDecoder().decode(
-        latestJson.data as unknown as ArrayBuffer
+        latestJsonBinary.data as unknown as ArrayBuffer
     );
+
+    const latestJson = JSON.parse(latestJsonString);
+
+    await storeReleaseAssetsInS3(latestRelease, latestJson);
+
+    for (const platform of Object.values(latestJson.platforms) as any) {
+        const url = new URL(platform.url);
+        const fileName = url.pathname
+            .split("/")
+            .at(-1)!
+            .replace(latestJson.version, "latest");
+        platform.url = `${S3_PUBLIC_URL}/releases/${fileName}`;
+    }
 
     const gistOctokit = new Octokit({
         auth: GIST_TOKEN,
@@ -60,12 +101,42 @@ export async function main() {
         gist_id: GIST_ID,
         files: {
             "draftgap-tauri-update.json": {
-                content: latestJsonString,
+                content: JSON.stringify(latestJson, null, 4),
             },
         },
     });
 
     console.log("Updated tauri update json");
+}
+
+export async function storeReleaseAssetsInS3(
+    release: Awaited<ReturnType<typeof octokit.repos.getLatestRelease>>,
+    latestJson: any
+) {
+    for (const asset of release.data.assets) {
+        console.log(`Storing ${asset.name} in S3`);
+        const res = await octokit.repos.getReleaseAsset({
+            owner: REPOSITORY_OWNER,
+            repo: REPOSITORY_NAME,
+            asset_id: asset.id,
+            headers: {
+                Accept: "application/octet-stream",
+            },
+        });
+
+        const assetBinary = res.data as unknown as ArrayBuffer;
+        const assetName = asset.name.replace(latestJson.version, "latest");
+
+        const params = {
+            Bucket: S3_BUCKET,
+            Key: `releases/${assetName}`,
+            Body: new Uint8Array(assetBinary),
+            ContentType: asset.content_type,
+        } satisfies PutObjectCommandInput;
+
+        const command = new PutObjectCommand(params);
+        await client.send(command);
+    }
 }
 
 main();
