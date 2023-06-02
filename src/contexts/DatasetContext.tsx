66b431b7f526bfaef17efc3f4c3a830a4cb55e2f
Vigo Vlugt <vigovlugt@gmail.com>
2023-06-02 21:23:20 +0200

    Remove console.logs

--- src/contexts/DatasetContext.tsx
+++ src/contexts/DatasetContext.tsx
@@ -1,7 +1,6 @@
 import {
     JSXElement,
     createContext,
-    createEffect,
     createResource,
     useContext,
 } from "solid-js";
@@ -9,14 +8,10 @@ import { DATASET_VERSION, Dataset } from "../lib/models/dataset/Dataset";
 
 const fetchDataset = async (name: "30-days" | "current-patch") => {
     try {
-        console.time(`fetch dataset ${name}`);
         const response = await fetch(
             `https://bucket.draftgap.com/datasets/v${DATASET_VERSION}/${name}.json`
         );
-        console.timeEnd(`fetch dataset ${name}`);
-        console.time(`parse dataset ${name}`);
         const json = await response.json();
-        console.timeEnd(`parse dataset ${name}`);
         return json as Dataset;
     } catch (err) {
         return undefined;
@@ -25,9 +20,6 @@ const fetchDataset = async (name: "30-days" | "current-patch") => {
 
 function createDatasetContext() {
     const [dataset] = createResource("current-patch", fetchDataset);
-    createEffect(() => {
-        console.log("dataset", dataset.state, dataset());
-    });
 
     const [dataset30Days] = createResource("30-days", fetchDataset);
 
