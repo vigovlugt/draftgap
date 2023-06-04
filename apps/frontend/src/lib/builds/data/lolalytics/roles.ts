c2136f73f176019d17aa7c90907d43ddff450215
Vigo Vlugt <vigovlugt@gmail.com>
2023-06-04 14:29:56 +0200

    Fix build

--- apps/frontend/src/lib/builds/data/lolalytics/roles.ts
+++
@@ -1,8 +0,0 @@
-export const LOLALYTICS_ROLES = [
-    "top",
-    "jungle",
-    "middle",
-    "bottom",
-    "support",
-] as const;
-export type LolalyticsRole = typeof LOLALYTICS_ROLES[number];
